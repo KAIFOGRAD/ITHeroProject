88897670da575f1b7adb7febd4beebd0b6f3d1fe
Vladislav Ostanin <ostanin8900@yandex.ru>
2025-04-25 13:10:49 +0500

    init commit

---
+++ geomap/src/main/java/com/ithero/geomap/GeomapApplication.java
@@ -0,0 +1,13 @@
+package com.ithero.geomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeomapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeomapApplication.class, args);
+	}
+
+}
